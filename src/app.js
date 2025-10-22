60ffccb10dfb231aaf0cc962537d51c8aa03bf41
Ziggy Chen <ziggycs9796@hotmail.com>
2025-10-22 17:04:26 +0800

    feat: app

--- src/app.js
+++ src/app.js
@@ -5,31 +5,40 @@ const app = express();
 
 app.use(express.json());
 
+// 在静态文件中间件之前设置 /sub 路径的响应类型为 text/plain
+app.use('/sub', (req, res, next) => {
+  res.type('text/plain;charset=utf-8');
+  next();
+});
 // 设置 /sub 目录为静态文件目录
-app.use('/sub', express.static(path.join(__dirname, 'sub')));
+app.use('/', express.static(path.join(__dirname, '../')));
 
 // 处理 POST 请求
-app.post('/save-text', (req, res) => {
-  const content = req.body.content; // 从请求体中获取文本内容
-  if (!content) {
-    return res.status(400).send('No content provided');
+app.post('/save', async(req, res) => {
+  const url = req.body.url; // 从请求体中获取文本内容
+  if (!url) {
+    return res.status(400).send('No url provided');
   }
 
-  const fileName = `file-${Date.now()}.txt`; // 根据时间戳生成文件名
-  const filePath = path.join(__dirname, 'sub', fileName);
+  const urlRes = await fetch(url)
+  const content = await urlRes.text()
+
+  const fileName = `sub`; // 根据时间戳生成文件名
+  const filePath = path.join(__dirname, '../', fileName);
 
   // 将文本内容保存到文件
   fs.writeFile(filePath, content, (err) => {
     if (err) {
+    console.log("🚀 ~ err:", err)
       return res.status(500).send('Error writing file');
     }
     // 返回文件的 URL 路径
-    res.send(`File saved successfully. Access it at /sub/${fileName}`);
+    res.send(`File saved successfully. Access it at /sub`);
   });
 });
 
 // 启动服务器
-const PORT = 3000;
+const PORT = 25502;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
